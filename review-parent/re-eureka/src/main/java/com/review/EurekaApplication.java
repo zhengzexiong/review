43c5ab5b27a148113d1301eec81c004d1fd31d6f
zhengzexiong <1187439506@qq.com>
2020-10-02 00:10:02 +0800

    注册中心

---
+++ review-parent/re-eureka/src/main/java/com/review/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 郑泽雄
+ * @Description Eureka启动类, 注册中心
+ * @create 2020-10-02 0:08
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
