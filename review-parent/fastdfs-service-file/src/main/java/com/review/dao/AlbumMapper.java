5fea195cb27ec33ba6afd0f265887afc0692f471
zhengzexiong <1187439506@qq.com>
2020-10-02 17:35:04 +0800

    相册DAO层

---
+++ review-parent/fastdfs-service-file/src/main/java/com/review/dao/AlbumMapper.java
@@ -0,0 +1,12 @@
+package com.review.dao;
+
+import com.review.pojo.Album;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author 郑泽雄
+ * @Description AlbumDAO层
+ * @create 2020-10-02 16:06
+ */
+public interface AlbumMapper extends Mapper<Album> {
+}
