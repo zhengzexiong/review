11cde9d705fc6ea023b0ac9228a9c5e59d4cdeff
zhengzexiong <1187439506@qq.com>
2020-10-02 17:37:24 +0800

    返回码，有利于查看程序运行情况

---
+++ review-parent/fastdfs-service-file/src/main/java/com/review/entity/StatusCode.java
@@ -0,0 +1,16 @@
+package com.review.entity;
+
+/**
+ * @author 郑泽雄
+ * @Description 返回码
+ * @create 2020-10-02 16:57
+ */
+public class StatusCode {
+    public static final int OK = 20000;//成功
+    public static final int ERROR = 20001;//失败
+    public static final int LOGINERROR = 20002;//用户名或密码错误
+    public static final int ACCESSERROR = 20003;//权限不足
+    public static final int REMOTEERROR = 20004;//远程调用失败
+    public static final int REPERROR = 20005;//重复操作
+    public static final int NOTFOUNDERROR = 20006;//没有对应的数据
+}
